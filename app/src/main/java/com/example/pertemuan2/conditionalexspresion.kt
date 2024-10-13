82d8ccdb61bad0d9570249d58ccb43bc017cc0e7
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-10-14 06:03:50 +0700

    menambah logika konditional sederhana dengan If-Else

--- app/src/main/java/com/example/pertemuan2/conditionalexspresion.kt
+++ app/src/main/java/com/example/pertemuan2/conditionalexspresion.kt
@@ -2,4 +2,12 @@ package com.example.pertemuan2
 fun ConditionalStatment() {
     println("== Conditional Expression ==")
 
+    // If-Else
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+    }else{
+        println("Negative Number")
+    }
+
 }
