73a0a421b43f1716b1ea495b5973dd0fdd3690d7
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-10-13 17:08:45 +0700

    menyimpan nilai string atau null dalam variable

--- app/src/main/java/com/example/pertemuan2/nullSafety.kt
+++ app/src/main/java/com/example/pertemuan2/nullSafety.kt
@@ -3,4 +3,13 @@ fun nullSafety() {
     //neverNull has String type
     var neverNull: String = "This can't be null"
 
+    // Throws a compieler error
+    // neverNull = null
+
+    // nullable has nullable String type
+    var nullable: String? = "You can keep a null here "
+    // This is OK
+    nullable = null
+
 }
+
