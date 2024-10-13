0efa62aa5147e5d1e5260e13d4ac32924cbe9773
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-10-13 18:02:39 +0700

    menambah contoh penggunaan lambda untuk mengubah string ke huruf kapitas

--- app/src/main/java/com/example/pertemuan2/LambdaFunction.kt
+++ app/src/main/java/com/example/pertemuan2/LambdaFunction.kt
@@ -5,7 +5,12 @@ package com.example.pertemuan2
 // Lambda functions biasanya digunakan untuk membuat fungsi yang sederhana
 // Lambda functions menggunakan tanda panah => untuk memisahkan parameter dan body fungsi
 
-fun uppercasesString(string: String): String {
+fun uppercaseString(string: String): String {
     return string.uppercase()
 
+}
+// Dapat ditulis dalam ekspresi lambda sbb :
+fun main() {
+    uppercaseString("Hello")
+    println({ string: String -> string.uppercase()} ("Hello"))
 }
