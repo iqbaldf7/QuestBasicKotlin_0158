94503be789bcb2af08bedf7d147a5213c2a6db49
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-10-13 15:26:05 +0700

    membuat Data Class membuat objek dari data class untuk menyimpan fungsi Equals

---
+++ app/src/main/java/com/example/pertemuan2/DataClass.kt
@@ -0,0 +1,14 @@
+package com.example.pertemuan2
+data class DataClass(
+    val id: Int,
+    var email: String
+)
+fun main() {
+    val data= DataClass(1, "Iqbal@gmail.com")
+    println(data)
+
+    // Fungsi Equals
+    val data2 = DataClass(2, "makan@gmail.com")
+    println(data == data2)
+
+}
