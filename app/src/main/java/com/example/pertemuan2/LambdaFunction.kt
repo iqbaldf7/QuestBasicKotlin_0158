3218fa7b10c1600370b466bbc9818b3c64f70d6b
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-10-13 17:57:51 +0700

    mengubah huruf dalam string menjdi hruf besar (uppercase)

---
+++ app/src/main/java/com/example/pertemuan2/LambdaFunction.kt
@@ -0,0 +1,11 @@
+package com.example.pertemuan2
+
+// Lambda Fungtions
+// Lambda Functions
+// Lambda functions biasanya digunakan untuk membuat fungsi yang sederhana
+// Lambda functions menggunakan tanda panah => untuk memisahkan parameter dan body fungsi
+
+fun uppercasesString(string: String): String {
+    return string.uppercase()
+
+}
