f2264bebeb1f0c06d84feedad4d8ffa746807751
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-10-14 07:04:02 +0700

    menambah objek contact dan akses pecetakan eamil dalam fungsi main()

---
+++ app/src/main/java/com/example/pertemuan2/Class.kt
@@ -0,0 +1,15 @@
+package com.example.pertemuan2
+class Motor()
+class Contact(val id: Int, var email: String)
+        fun main() {
+            val contact = Contact(1, "mary@gmail.com")
+            //prints the value of the property: email
+            println(contact.email)
+
+            //Updates the value of the property: email
+            contact.email = "jane@gmail.com"
+
+            //prints the value of the property: email
+            println(contact.email)
+
+        }
