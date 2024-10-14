beb6982f6e88660d9c2385598d11a89f0813fbb1
iqbaldf7 <Iqbaldathonah10@gmail.com>
2024-10-14 07:37:18 +0700

    menambah objek data class

---
+++ app/src/main/java/com/example/pertemuan2/DataClass.kt
@@ -0,0 +1,27 @@
+package com.example.pertemuan2
+data class DataClass(
+    val id: Int,
+    var email: String
+)
+fun main() {
+    val data= DataClass(1, "indra@gmail.com")
+    println(data)
+
+    // Fungsi Equals
+    val data2 = DataClass(2, "makan@gmail.com")
+    println(data == data2)
+
+    //Fungsi Copy
+    val data3 = data.copy()
+    println(data3)
+
+    //Fungsi copy dengan perubahan
+    val data4 = data.copy(email = "sahar@gmail.com")
+    println(data4)
+
+}
+
+
+
+
+
